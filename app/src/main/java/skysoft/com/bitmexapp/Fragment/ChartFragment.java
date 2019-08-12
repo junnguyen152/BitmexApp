f6542b39dea6c6f45ce36c0fc6c926a794665f3c
junnguyen152 <junnguyen152@gmai.com>
2019-08-12 11:44:43 +0700

    Thêm biểu đồ MPAndroidChart (test)

--- app/src/main/java/skysoft/com/bitmexapp/Fragment/ChartFragment.java
+++ app/src/main/java/skysoft/com/bitmexapp/Fragment/ChartFragment.java
@@ -1,14 +1,31 @@
 package skysoft.com.bitmexapp.Fragment;
 
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
+
+import java.util.ArrayList;
+
+import skysoft.com.bitmexapp.Adapter.MyMarkerView;
 import skysoft.com.bitmexapp.R;
 
 
@@ -17,6 +34,8 @@ import skysoft.com.bitmexapp.R;
  */
 public class ChartFragment extends Fragment {
 
+    public LineChart buyChart;
+    public LineChart sellChart;
 
     public ChartFragment() {
         // Required empty public constructor
@@ -33,5 +52,107 @@ public class ChartFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        buyChart = requireActivity().findViewById(R.id.buy_chart);
+        sellChart = requireActivity().findViewById(R.id.sell_chart);
+
+        buyChart.setTouchEnabled(true);
+        buyChart.setPinchZoom(true);
+        MyMarkerView mv = new MyMarkerView(requireContext(), R.layout.custom_market_view);
+
+        mv.setChartView(buyChart);
+        buyChart.setMarker(mv);
+        renderData();
+    }
+
+    public void renderData() {
+        LimitLine llXAxis = new LimitLine(10f, "Index 10");
+        llXAxis.setLineWidth(4f);
+        llXAxis.enableDashedLine(10f, 10f, 0f);
+        llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        llXAxis.setTextSize(10f);
+        llXAxis.setTextColor(Color.WHITE);
+
+        XAxis xAxis = buyChart.getXAxis();
+        xAxis.enableGridDashedLine(10f, 10f, 0f);
+        xAxis.setAxisMaximum(10f);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setTextColor(Color.WHITE);;
+        xAxis.setDrawLimitLinesBehindData(true);
+
+        LimitLine ll1 = new LimitLine(215f, "Maximum Limit");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        ll1.setTextSize(10f);
+        ll1.setTextColor(Color.WHITE);
+
+        LimitLine ll2 = new LimitLine(70f, "Minimum Limit");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        ll2.setTextSize(10f);
+        ll2.setTextColor(Color.WHITE);
+
+        YAxis leftAxis = buyChart.getAxisLeft();
+        leftAxis.removeAllLimitLines();
+        leftAxis.addLimitLine(ll1);
+        leftAxis.addLimitLine(ll2);
+        leftAxis.setAxisMaximum(350f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawZeroLine(false);
+        leftAxis.setDrawLimitLinesBehindData(false);
+        leftAxis.setTextColor(Color.WHITE);
+
+        buyChart.getAxisRight().setEnabled(false);
+        setData();
+    }
+
+    public void setData() {
+        ArrayList<Entry> values = new ArrayList<>();
+        values.add(new Entry(1, 50));
+        values.add(new Entry(2, 100));
+        values.add(new Entry(3, 80));
+        values.add(new Entry(4, 120));
+        values.add(new Entry(5, 110));
+        values.add(new Entry(7, 150));
+        values.add(new Entry(8, 250));
+        values.add(new Entry(9, 190));
+
+        LineDataSet set1;
+        if (buyChart.getData() != null &&
+                buyChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) buyChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            buyChart.getData().notifyDataChanged();
+            buyChart.notifyDataSetChanged();
+        } else {
+            set1 = new LineDataSet(values, "Sample Data");
+            set1.setDrawIcons(false);
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.parseColor("#f5bc00"));
+            set1.setCircleColor(Color.parseColor("#f5bc00"));
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setValueTextColor(Color.parseColor("#f5bc00"));
+            set1.setDrawFilled(true);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+
+            if (Utils.getSDKInt() >= 18) {
+                Drawable drawable = ContextCompat.getDrawable(requireContext(), R.drawable.fade_buy);
+                set1.setFillDrawable(drawable);
+            } else {
+                set1.setFillColor(Color.parseColor("#f5bc00"));
+            }
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+            LineData data = new LineData(dataSets);
+            buyChart.setData(data);
+        }
     }
 }
